4d21f5cc0f219320ec7243def79e3b322cbba454
Timber1900 <timber.gamer@gmail.com@>
2020-09-19 17:14:21 +0100

    Fully operational (and fast) 2D ray casting for 1000 lines in preparation for 3d

--- RaycasterWithOpentk/Game.cs
+++ RaycasterWithOpentk/Game.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
 using OpenTK;
 using OpenTK.Graphics;
 using OpenTK.Graphics.ES11;
@@ -15,11 +16,11 @@ namespace RaycasterWithOpentk
         private readonly int[] _gameBoard =
         {
             1, 1, 1, 1, 1, 1, 1, 1,
+            1, 0, 1, 0, 0, 0, 0, 1,
+            1, 0, 1, 0, 0, 0, 0, 1,
+            1, 0, 1, 0, 0, 0, 0, 1,
             1, 0, 0, 0, 0, 0, 0, 1,
-            1, 0, 0, 0, 0, 0, 0, 1,
-            1, 0, 0, 0, 0, 0, 0, 1,
-            1, 0, 0, 0, 0, 0, 0, 1,
-            1, 0, 0, 0, 0, 0, 0, 1,
+            1, 0, 0, 0, 0, 1, 0, 1,
             1, 0, 0, 0, 0, 0, 0, 1,
             1, 1, 1, 1, 1, 1, 1, 1
         };
@@ -52,9 +53,9 @@ namespace RaycasterWithOpentk
             _player = new Player
             {
                 Pos = new Vector2(500, 500),
-                LookDir = new Vector2(0, 1),
-                Right = new Vector2(1, 0),
-                LookAngle = (float) Math.PI / 2
+                LookDir = new Vector2(0, -1),
+                Right = new Vector2(-1, 0),
+                LookAngle =  3f * (float)Math.PI / 2
             };
             _xsize = 8;
             _ysize = 8;
@@ -65,7 +66,9 @@ namespace RaycasterWithOpentk
         {
             Clear();
             RenderGameBoard(_gameBoard);
+            RayCast(_player, (float) Math.PI / 2);
             RenderPlayer(_player);
+
             base.OnRenderFrame(e);
 
         }
@@ -74,7 +77,9 @@ namespace RaycasterWithOpentk
         {
             var input = Keyboard.GetState();
             const float playerSpeed = 100f;
-            const float sensitivity = 0.01f;
+            const float sensitivity = 0.001f;
+            if (Focused)
+            {
                 if (input.IsKeyDown(Key.W))
                 {
                     _player.Pos += _player.LookDir * playerSpeed * (float) e.Time; // Forward
@@ -110,17 +115,20 @@ namespace RaycasterWithOpentk
                     _lastPos = new Vector2(mouse.X, mouse.Y);
 
                     // Apply the camera pitch and yaw (we clamp the pitch in the camera class)
-                    _player.LookAngle -= deltaX * sensitivity;
+                    _player.LookAngle += deltaX * sensitivity;
 
                     if (_player.LookAngle > (float) Math.PI * 2) { _player.LookAngle -= (float) Math.PI * 2; }
                     if (_player.LookAngle < 0) { _player.LookAngle += (float) Math.PI * 2; }
-                    
+                
                     _player.LookDir = new Vector2((float) Math.Cos(_player.LookAngle), (float) Math.Sin(_player.LookAngle));
                     _player.Right = new Vector2((float) Math.Sin(_player.LookAngle), (float) -Math.Cos(_player.LookAngle));
                 }
 
                 Mouse.SetPosition(1920 / 2, 1080 / 2);
-
+            }
+            
+            
+            
             base.OnUpdateFrame(e);
         }
 
@@ -150,7 +158,140 @@ namespace RaycasterWithOpentk
         private void RenderPlayer(Player player)
         {
             drawEllipse(player.Pos.X, player.Pos.Y, 10,10, new Color4(0f,0f,1f,1f));
-            drawLine(player.Pos.X, -(player.Pos.Y - Height),player.Pos.X + (player.LookDir.X * 25), -(player.Pos.Y - Height) - (player.LookDir.Y * 25), new Color4(1f, 0f,0f,1f));
+            drawLine(player.Pos.X, player.Pos.Y ,player.Pos.X + (player.LookDir.X * 25), player.Pos.Y + (player.LookDir.Y * 25), new Color4(1f, 0f,0f,1f));
+        }
+
+        
+        private void RayCast(Player player, float fov)
+        {
+            int r, mx, my, mp, dof;
+            float rx, ry, ra, xo, yo;
+
+            ra = player.LookAngle - fov / 2;
+            if (ra > Math.PI * 2) { ra -= (float)Math.PI * 2; }
+            if (ra < 0) { ra += (float)Math.PI * 2; }
+            int numRays = 1000;
+            float step = fov / numRays;
+            for (r = 0; r < numRays; r++)
+            {
+                dof = 0;
+                float distH = 1000000f, hx = player.Pos.X, hy = player.Pos.Y;
+                float aTan = -1 / (float) Math.Tan(ra);
+                if (ra > Math.PI)
+                {
+                    ry = (float)Math.Floor(player.Pos.Y / (Height / _ysize) - 0.01) * (Height / _ysize);
+                    rx = Math.Abs(player.Pos.Y - ry) * aTan + player.Pos.X;
+                    yo = -(Height / _ysize);
+                    xo = -yo * aTan;
+                }
+                else if (ra < Math.PI)
+                {
+                    ry = (float)Math.Floor(player.Pos.Y / (Height / _ysize) + 1.01) * (Height / _ysize);
+                    rx = -Math.Abs(player.Pos.Y - ry) * aTan + player.Pos.X;
+                    yo = (Height / _ysize);
+                    xo = -yo * aTan;
+                }
+                else
+                {
+                    rx = player.Pos.X;
+                    ry = player.Pos.Y;
+                    yo = 0;
+                    xo = 0;
+                    dof = 8;
+                }
+                while (dof < 8)
+                {
+                    mx = (int) rx / (Width / _xsize);
+                    my = (int) ry / (Height / _ysize);
+                    mp = my * _xsize + mx;
+                    if (mp < _gameBoard.Length - 1 && mp > 0)
+                    {
+                        if (_gameBoard[mp] == 1 || _gameBoard[mp - _xsize] == 1)
+                        {
+                            var temp = new Vector2(rx - player.Pos.X, ry - player.Pos.Y);
+                            distH = temp.Length;
+                            hx = rx;
+                            hy = ry;
+                            break;
+                        }
+                    }
+
+                    rx += xo;
+                    ry += yo;
+                    dof += 1;
+   
+                }
+                
+                dof = 0;
+                float distV = 1000000f, vx = player.Pos.X, vy = player.Pos.Y;
+                float nTan = -(float)Math.Tan(ra);
+                if (ra > Math.PI / 2 && ra < (Math.PI * 3) / 2)
+                {
+                    rx = (float)Math.Floor(player.Pos.X / (Width / _xsize) - 0.01) * (Width / _xsize);
+                    ry = Math.Abs(player.Pos.X - rx) * nTan + player.Pos.Y;
+                    xo = -(Width / _xsize);
+                    yo = -xo * nTan;
+                }
+                else if (ra < Math.PI / 2 || ra > (Math.PI * 3) / 2)
+                {
+                    rx = (float)Math.Floor(player.Pos.X / (Width / _xsize) + 1.01) * (Width / _xsize);
+                    ry = -Math.Abs(player.Pos.X - rx) * nTan + player.Pos.Y;
+                    xo = (Width / _xsize);
+                    yo = -xo * nTan;
+                }
+                else
+                {
+                    rx = player.Pos.X;
+                    ry = player.Pos.Y;
+                    yo = 0;
+                    xo = 0;
+                    dof = 8;
+                }
+                while (dof < 8)
+                {
+                    mx = (int) rx / (Width / _xsize);
+                    my = (int) ry / (Height / _ysize);
+                    mp = my * _xsize + mx;
+                    if (mp < _gameBoard.Length - 1 && mp > 0)
+                    {
+                        if (_gameBoard[mp] == 1 || _gameBoard[mp - 1] == 1)
+                        {
+                            var temp = new Vector2(rx - player.Pos.X, ry - player.Pos.Y);
+                            distV = temp.Length;
+                            vx = rx;
+                            vy = ry;
+                            break;
+                        }
+                        
+                    }
+                    
+                    rx += xo;
+                    ry += yo;
+                    dof += 1;
+   
+                }
+
+                
+                if(distV<=distH)
+                {
+                    rx = vx;
+                    ry = vy;
+                }
+                if(distH<distV)
+                {
+                    rx = hx;
+                    ry = hy;
+                }
+                drawLine(player.Pos.X, player.Pos.Y, rx, ry, new Color4(0f, 0.3f, 0.7f, 0.5f));  
+                //drawEllipse(rx, ry, 5, 5, new Color4(1f, 0f, 0f, 0.5f));
+                
+
+                ra += step;
+
+                if (ra > Math.PI * 2) { ra -= (float)Math.PI * 2; }
+                if (ra < 0) { ra += (float)Math.PI * 2; }
+            }
+            
         }
     }
 }
